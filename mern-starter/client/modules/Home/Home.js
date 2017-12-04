d96741ac046a36af0a144cb1752518aac6e2430b
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-12-05 00:29:59 +0100

    Add module Home

--- mern-starter/client/modules/Home/Home.js
+++ mern-starter/client/modules/Home/Home.js
@@ -2,15 +2,15 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-// Import Style
-import styles from './Home.css';
-
 class Home extends Component {
   render() {
     return (
-    );
+      <div>
+        <h2>Hello world!</h2>
+      </div>
+    )
   }
-}
+};
 
 const mapStateToProps = (state) => {
   return {};
